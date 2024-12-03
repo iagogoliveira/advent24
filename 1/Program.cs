035d9d597b3e7ec0c1267ff38cde321d0ed79594
iago <iagogoliver@gmail.com>
2024-12-02 23:53:57 -0300

    Push Codigos dias 1, 2

---
+++ 1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+
+
+List<int> esquerda = new List<int>();
+List<int> direita = new List<int>();
+
+using (StreamReader nr = new StreamReader("E:\\adventofcode\\Advent2024\\1\\ex1.txt"))
+{
+    string line;
+    while ((line = nr.ReadLine()) != null)
+    {
+        string[] parts = line.Split('-');
+
+        if (parts.Length == 2)
+        {
+            esquerda.Add(int.Parse(parts[0]));
+            direita.Add(int.Parse(parts[1]));
+        }
+
+    }
+}
+
+
+esquerda.Sort();
+direita.Sort();
+
+int soma = 0;
+int numEsq;
+int numDir;
+
+for (int i = 0; i < direita.Count; i++)
+{
+    for (int j = 0; j < esquerda.Count; j++)
+    {
+        numEsq = esquerda[j];
+        numDir = direita[j];
+
+        Console.WriteLine($"{numDir} - {numEsq}");
+
+        soma += Math.Abs(Math.Abs(numDir) - Math.Abs(numEsq));
+
+    }
+    break;
+}
+
+Console.WriteLine("Esquerda: " + string.Join(", ", esquerda));
+Console.WriteLine("Direita: " + string.Join(", ", direita));
+
+
+
+Console.WriteLine("Soma: " + soma);
+
---
+++ 2/Program.cs
@@ -0,0 +1,51 @@
+﻿int soma = 0;
+
+using (StreamReader nr = new StreamReader("E:\\adventofcode\\Advent2024\\2\\ex2.txt"))
+{
+    string line;
+    while ((line = nr.ReadLine()) != null)
+    {
+        int[] listaInt = line.Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+        if (ValidacaoProcesso(listaInt))
+        {
+            soma += 1;
+        }
+
+    }
+}
+
+Console.WriteLine("Soma de processos seguros: " + soma);
+
+bool ValidacaoProcesso(int[] reports)
+{
+    bool validacao = false;
+    bool crescente = false;
+    bool decrescente = false;
+    int[] diferenca = new int[0];
+
+    for (int i = 0; i < reports.Length - 1; i++)
+    {
+        diferenca = diferenca.Append(Math.Abs(reports[i + 1] - reports[i])).ToArray();
+    }
+
+    for (int i = 0; i < reports.Length - 1; i++)
+    {
+        if (reports[i] <= reports[i + 1])
+        {
+            crescente = true;
+        }
+        if (reports[i] >= reports[i + 1])
+        {
+            decrescente = true;
+        }
+    }
+    if (crescente != decrescente)
+    {
+        if (diferenca.Max() <= 3)
+        {
+            return true;
+        }
+    }
+    return validacao;
+}
